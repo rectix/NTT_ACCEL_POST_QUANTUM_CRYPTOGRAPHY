39c0ed8f959f52a4dd7bd058e7a7d47fc85d749e
rectix <76615506+rectix@users.noreply.github.com>
2025-04-17 01:33:08 +0200

    Add files via upload

--- modulo_12b.v
+++ modulo_12b.v
@@ -25,11 +25,9 @@ module modulo_12b #(parameter WIDTH  =12 ,
             //      FSM  STATES
             //==================================================================
 
-                    parameter IDLE   = 2'b00,
-                    parameter CALC  = 2'b01,
-                    parameter CHECK  = 2'b10,
-                    parameter ISSUE  = 2'b11
-                         
+                    parameter STAGE1  = 1'b0,
+                    parameter STAGE2  = 1'b1
+
 )(
     input wire clock_i,
     input wire nreset_i,
@@ -48,21 +46,18 @@ module modulo_12b #(parameter WIDTH  =12 ,
     //==================================================================
     
     
-    reg [1:0]  futur;
+    reg   futur;
    
     
     
     
-    reg [WIDTH-1:0]  k_shift, n,m;
-    reg [WIDTH-1:0]  tmp;
-    reg [WIDTH-1:0]  result_rg; 
-    
+    reg [WIDTH-1:0]  k_shift;  
     reg [WIDTH-1:0]  a_rg   ;
     
     //==================================================================
     //      OUTPUT ASSIGNEMENT 
     //==================================================================
-    assign res_o = result_rg;
+    assign res_o = a_rg;
     
     
 
@@ -76,58 +71,36 @@ module modulo_12b #(parameter WIDTH  =12 ,
     if(!nreset_i)  begin 
     
         a_rg        <= {WIDTH{1'b0}};
-        result_rg   <= {WIDTH{1'b0}};
-        tmp         <= q_i;
         k_shift     <= {WIDTH{1'b0}};
-        n           <= {WIDTH{1'b0}};
+
             
-        futur       <= IDLE ; 
+        futur       <= STAGE1 ; 
     
     end else  begin
 
     case(futur)
     
-    IDLE: begin
-        result_rg <= 12'b0;  
-        
+    STAGE1: begin
+      
         k_shift     <=  k_shift  + 1'b1; 
-        futur  <= CALC; 
+        futur  <= STAGE2; 
         
         end 
         
         
         
                 
-    CALC : begin
-        result_rg <= 12'b0; 
-
-        if((k_shift >> 1) & 1)begin 
-            tmp <= (q_i << (k_shift >> 1)) + q_i;
-            futur <= CHECK; 
-        
-        end else begin
-            tmp <= (q_i << (k_shift >> 1));
-            futur <= CHECK; 
-        end 
-        
-
-        end 
-        
-    CHECK: begin
-        a_rg <= a_i - tmp;
-        result_rg <= 12'b0;
-        
-        if(a_rg  > (q_i << 1)) begin  futur  <=  IDLE;  end
-        else begin result_rg <= a_rg - q_i; end  
-      
+    STAGE2 : begin
+        a_rg <= a_i - (q_i << (k_shift >> 1)) - q_i;  
+        if(a_rg  > q_i ) begin  futur  <=  STAGE1;  end
+            
         end 
  
-       
 
-    default: result_rg <= a_i;
+    default: a_rg <= a_i;
     
     endcase
-    end
+    end  //nreset
     end //always 
     
 
